7bf0b54c2dcab94ef8bac7f98a7fd2843266cc2f
unknown <andreyandreydn16@gmail.com>
2024-03-02 13:10:37 +0300

    some add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import { Shitting } from "./Shitting";
 
 function App() {
+  const [isClick, setIsClick] = useState(false);
+
+  const handleClick = () => {
+    setIsClick(!isClick);
+  };
+
   return (
-    <div className="App">
-      тут мое приложение и оно работает.
+    <div className="app">
+      <span className="title"> тут мое приложение и оно работает.</span>
+      <button className="btn" onClick={handleClick}/>
+      <aside className="aside"></aside>
       <Shitting />
+      <div>
+        <>
+          {isClick ? (
+            <div className="zopa">Привет!</div>
+          ) : (
+            <div className="zopaa">Иди нахуй</div>
+          )}
+        </>
+      </div>
     </div>
   );
 }
